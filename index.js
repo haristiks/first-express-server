96c866cc2bb88709b788feae4f1471b75aa62138
haristiks <nklharis@gmail.com>
2023-09-02 13:10:26 +0530

    first-commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const path= require("path")
+
+app.get("/",(req,res)=>{
+    res.status(200)
+    res.sendFile(path.join(__dirname, "home.html"))
+})
+
+app.get("/contacts",(req,res)=>{
+    res.status(200)
+    res.send("<h1>Contacts<h1/>")
+})
+
+app.get("*",(req,res)=>{
+    res.status(404)
+    res.send("<h1>404<h1/>")
+})
+
+
+
+app.listen(3000,()=>{
+    console.log("server running on port 3000");
+})
